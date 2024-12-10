fc40e389e8f723f3f4ee3a1001f56d696b0c1e2f
pmpknu <dangorlyakov@gmail.com>
2024-12-10 11:10:03 +0300

    feat(multiset): add `add` function

--- lib/bt_bag.ml
+++ lib/bt_bag.ml
@@ -3,6 +3,9 @@ module type Bag = sig
     type 'a bag
     val empty : 'a bag
     val is_empty : 'a bag -> bool
+
+    val add : 'a bag -> key -> 'a bag
+    (* add bag x adds x to the bag, returning an element that can later be removed from the bag. add runs in constant time *)
 end
 
 module type OrderedType = sig
@@ -13,7 +16,6 @@ end
 
 module Make (Ord : Set.OrderedType) : Bag with type key = Ord.t = struct
     type key = Ord.t
-    let compare = Ord.compare
 
     type 'a bag = Leaf | Node of 'a bag * key * int * 'a bag
 
@@ -21,4 +23,15 @@ module Make (Ord : Set.OrderedType) : Bag with type key = Ord.t = struct
     let is_empty = function
         | Leaf -> true
         | _ -> false
+
+    let rec add bag x = 
+      match bag with
+      | Leaf -> Node (Leaf, x, 1, Leaf)
+      | Node (left, key, count, right) ->
+          if x = key then
+            Node (left, key, count + 1, right)
+          else if Ord.compare x key < 0 then
+            Node (add left x, key, count, right)
+          else
+            Node (left, key, count, add right x)
 end
