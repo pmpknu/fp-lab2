3c4d8a1acecea8f834771035a910b532ffdf0b31
pmpknu <dangorlyakov@gmail.com>
2024-12-10 08:59:37 +0300

    feat(bag): add empty implementation

---
+++ lib/bt_bag.ml
@@ -0,0 +1,23 @@
+module type Bag = sig
+    type key
+    type 'a bag
+    val empty : 'a bag
+    val is_empty : 'a bag -> bool
+end
+
+module type OrderedType = sig
+  type t
+
+  val compare : t -> t -> int
+end
+
+module Make (Ord : Set.OrderedType) : Bag with type key = Ord.t = struct
+    type key = Ord.t
+
+    type 'a bag = Leaf | Node of 'a bag * key * 'a * 'a bag
+
+    let empty = Leaf
+    let is_empty = function
+        | Leaf -> true
+        | _ -> false
+end
