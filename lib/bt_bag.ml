0145e6e9248c42a6f6c794969593140d78b8f06b
pmpknu <dangorlyakov@gmail.com>
2024-12-10 10:20:15 +0300

    fix(multiset): move from set to multiset

--- lib/bt_bag.ml
+++ lib/bt_bag.ml
@@ -14,7 +14,7 @@ end
 module Make (Ord : Set.OrderedType) : Bag with type key = Ord.t = struct
     type key = Ord.t
 
-    type 'a bag = Leaf | Node of 'a bag * key * 'a * 'a bag
+    type 'a bag = Leaf | Node of 'a bag * key * 'a list * 'a bag
 
     let empty = Leaf
     let is_empty = function
