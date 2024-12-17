f0ed284120ea7e122f9d2c0a287fcd212a2a4ea6
pmpknu <dangorlyakov@gmail.com>
2024-12-17 16:28:19 +0300

    refactor: take module IntBag out

--- test/test_bt_bag.ml
+++ test/test_bt_bag.ml
@@ -1,10 +1,11 @@
 open Alcotest
 
+module IntBag = Bt_bag.Make(struct
+    type t = int
+    let compare = compare
+  end)
+
 let test_add_case () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -13,23 +14,15 @@ let test_add_case () =
   let bag = IntBag.add 2 bag in
   let result = IntBag.elements bag in
   let expected = [1; 1; 2; 2; 3] in
-  check (list int) "same elements" expected result
+  check (list int) "same elements in add" expected result
 
 let test_is_empty_case () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   check bool "empty bag" true (IntBag.is_empty bag);
   let bag = IntBag.add 1 bag in
   check bool "non-empty bag" false (IntBag.is_empty bag)
 
 let test_find_case () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -42,10 +35,6 @@ let test_find_case () =
   check_raises "find 4" Not_found (fun () -> ignore (IntBag.find 4 bag))
 
 let test_find_opt_case () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -58,10 +47,6 @@ let test_find_opt_case () =
   check (option int) "find 4" None (IntBag.find_opt 4 bag)
 
 let test_merge () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag1 = IntBag.empty in
   let bag1 = IntBag.add 1 bag1 in
   let bag1 = IntBag.add 2 bag1 in
@@ -77,13 +62,9 @@ let test_merge () =
   let bag = IntBag.merge bag1 bag2 in
   let result = IntBag.elements bag in
   let expected = [1; 1; 1; 2; 2; 2; 2; 3; 3; 3] in
-  check (list int) "same elements" expected result
+  check (list int) "same elements in merge" expected result
 
 let test_remove () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -93,13 +74,9 @@ let test_remove () =
   let bag = IntBag.remove 1 bag in
   let result = IntBag.elements bag in
   let expected = [1; 2; 2; 3] in
-  check (list int) "same elements" expected result
+  check (list int) "same elements in remove" expected result
 
 let test_map () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -109,13 +86,9 @@ let test_map () =
   let bag = IntBag.map (fun x -> x + 1) bag in
   let result = IntBag.elements bag in
   let expected = [2; 2; 3; 3; 4] in
-  check (list int) "same elements" expected result
+  check (list int) "same elements in map" expected result
 
 let test_filter () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -125,13 +98,9 @@ let test_filter () =
   let bag = IntBag.filter (fun x -> x mod 2 = 0) bag in
   let result = IntBag.elements bag in
   let expected = [2; 2] in
-  check (list int) "same elements" expected result
+  check (list int) "same elements in filter" expected result
 
 let test_fold_right () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -140,13 +109,23 @@ let test_fold_right () =
   let bag = IntBag.add 2 bag in
   let result = IntBag.fold_right (fun key count acc -> acc + key * count) bag 0 in
   let expected = 1 * 2 + 2 * 2 + 3 * 1 in
-  check int "same elements" expected result
+  check int "same elements in fold_right" expected result
+
+let test_fold_right_zero_elements () =
+  let bag = IntBag.empty in
+  let result = IntBag.fold_right (fun key count acc -> acc + key * count) bag 0 in
+  let expected = 0 in
+  check int "same elements in fold right with no elements" expected result
+
+let test_fold_right_two_elements () =
+  let bag = IntBag.empty in
+  let bag = IntBag.add 1 bag in
+  let bag = IntBag.add 2 bag in
+  let result = IntBag.fold_right (fun key count acc -> acc - key * count) bag 0 in
+  let expected = 1 - (2 - 0) in
+  check int "same elements in fold_right with two elements with substraction function" expected result
 
 let test_fold_left () = 
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
@@ -158,20 +137,16 @@ let test_fold_left () =
   check int "same elements" expected result
 
 let test_fold_left_right_difference_case () =
-  let module IntBag = Bt_bag.Make(struct
-      type t = int
-      let compare = compare
-    end) in
   let bag = IntBag.empty in
   let bag = IntBag.add 5 bag in
   let bag = IntBag.add 10 bag in
   let bag = IntBag.add 15 bag in
   let result_left = IntBag.fold_left (fun acc key count -> acc - key * count) 0 bag in
-  (*let result_right = IntBag.fold_right (fun key count acc -> acc - key * count) bag 0 in*)
+  let result_right = IntBag.fold_right (fun key count acc -> acc - key * count) bag 0 in
   let expected_left = ((0 - 5) - 10) - 15 in
-  (* let expected_right = 5 - (10 - (15 - 0)) in *)
-  check int "same left" expected_left result_left
-  (* check int "same right" expected_right result_right *)
+  let expected_right = 5 - (10 - (15 - 0)) in
+  check int "same left" expected_left result_left;
+  check int "same right" expected_right result_right
 
 let () =
   run "bt_bag tests" [
@@ -210,6 +185,8 @@ let () =
       "test_fold",
       [
         test_case "test_fold_right" `Quick test_fold_right;
+        test_case "test_fold_right_zero_elements" `Quick test_fold_right_zero_elements;
+        test_case "test_fold_right_two_elements" `Quick test_fold_right_two_elements;
         test_case "test_fold_left" `Quick test_fold_left;
         test_case "test_fold_left_right_difference" `Quick test_fold_left_right_difference_case
       ]
