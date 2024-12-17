78953da6c5578159b9b00704c374e0f5c4247249
pmpknu <dangorlyakov@gmail.com>
2024-12-17 20:20:56 +0300

    feat: try to run QCheck tests

---
+++ test/test_property.ml
@@ -0,0 +1,17 @@
+module IntBag = Bt_bag.Make (struct
+  type t = int
+  let compare = compare
+end)
+
+let prop_reverse lst = (lst) = lst;;
+
+open QCheck
+
+let test =
+  QCheck.Test.make
+  ~count:1000
+  ~name:"reverse"
+  (list small_int)
+  (fun x -> prop_reverse x);;
+
+QCheck_runner.run_tests ~verbose:true [test];;
