2cd0d394bc0409dc341f1fbe0928c006901c56e3
pmpknu <dangorlyakov@gmail.com>
2024-12-17 18:17:39 +0300

    typo: dune fmt

--- lib/bt_bag.ml
+++ lib/bt_bag.ml
@@ -1,28 +1,19 @@
 module type Bag = sig
-    type key
-    type 'a btree
-    val empty : 'a btree
-    val is_empty : 'a btree -> bool
+  type key
+  type 'a btree
 
-    val add : key -> 'a btree -> 'a btree
-
-    val elements : 'a btree -> key list
-
-    val find : key -> 'a btree -> int
-
-    val find_opt : key -> 'a btree -> int option
-
-    val merge : 'a btree -> 'a btree -> 'a btree
-
-    val remove : key -> 'a btree -> 'a btree
-
-    val map : (key -> key) -> 'a btree -> 'a btree
-
-    val filter : (key -> bool) -> 'a btree -> 'a btree
-
-    val fold_left : ('a -> key -> int -> 'a) -> 'a -> 'a btree -> 'a
-
-    val fold_right : (key -> int -> 'a -> 'a) -> 'a btree -> 'a -> 'a
+  val empty : 'a btree
+  val is_empty : 'a btree -> bool
+  val add : key -> 'a btree -> 'a btree
+  val elements : 'a btree -> key list
+  val find : key -> 'a btree -> int
+  val find_opt : key -> 'a btree -> int option
+  val merge : 'a btree -> 'a btree -> 'a btree
+  val remove : key -> 'a btree -> 'a btree
+  val map : (key -> key) -> 'a btree -> 'a btree
+  val filter : (key -> bool) -> 'a btree -> 'a btree
+  val fold_left : ('a -> key -> int -> 'a) -> 'a -> 'a btree -> 'a
+  val fold_right : (key -> int -> 'a -> 'a) -> 'a btree -> 'a -> 'a
 end
 
 module type OrderedType = sig
@@ -32,88 +23,113 @@ module type OrderedType = sig
 end
 
 module Make (Ord : Set.OrderedType) : Bag with type key = Ord.t = struct
-    type key = Ord.t
+  type key = Ord.t
 
-    type 'a btree = Empty | Node of {left:'a btree; value:key; count:int; right:'a btree}
+  type 'a btree =
+    | Empty
+    | Node of
+        { left : 'a btree
+        ; value : key
+        ; count : int
+        ; right : 'a btree
+        }
 
-    let empty = Empty
-    let is_empty = function
-        | Empty -> true
-        | _ -> false
+  let empty = Empty
 
-    let rec add x bag = 
-      match bag with
-      | Empty -> Node {left = Empty; value = x; count = 1; right = Empty}
-      | Node {left; value = key; count; right} ->
-          if Ord.compare x key = 0 then
-            Node {left; value = key; count = count + 1; right}
-          else if Ord.compare x key < 0 then
-            Node {left = add x left; value = key; count; right}
-          else
-            Node {left; value = key; count; right = add x right}
+  let is_empty = function
+    | Empty -> true
+    | _ -> false
+  ;;
 
-    let rec elements = function
-      | Empty -> []
-      | Node {left; value; count; right} ->
-          (elements left) @ (List.init count (fun _ -> value)) @ (elements right)
-  
-    let rec find x = function
-        Empty -> raise Not_found
-      | Node {left; value; count; right} ->
-          match Ord.compare x value with
-          | 0 -> count
-          | n when n < 0 -> find x left
-          | _ -> find x right
+  let rec add x bag =
+    match bag with
+    | Empty -> Node { left = Empty; value = x; count = 1; right = Empty }
+    | Node { left; value = key; count; right } ->
+      if Ord.compare x key = 0
+      then Node { left; value = key; count = count + 1; right }
+      else if Ord.compare x key < 0
+      then Node { left = add x left; value = key; count; right }
+      else Node { left; value = key; count; right = add x right }
+  ;;
 
-    let rec find_opt x = function
-        Empty -> None
-      | Node {left; value; count; right} ->
-          match Ord.compare x value with
-          | 0 -> Some count
-          | n when n < 0 -> find_opt x left
-          | _ -> find_opt x right
+  let rec elements = function
+    | Empty -> []
+    | Node { left; value; count; right } ->
+      elements left @ List.init count (fun _ -> value) @ elements right
+  ;;
 
-    let merge t1 t2 =
-      let t2_values = elements t2 in
-      let add_to_tree t k = add k t in
-      List.fold_left add_to_tree t1 t2_values
+  let rec find x = function
+    | Empty -> raise Not_found
+    | Node { left; value; count; right } ->
+      (match Ord.compare x value with
+       | 0 -> count
+       | n when n < 0 -> find x left
+       | _ -> find x right)
+  ;;
 
-    let rec remove x = function
-      | Empty -> Empty
-      | Node {left; value; count; right} ->
-          match Ord.compare x value with
-          | 0 -> if count = 1 then merge left right else Node {left; value; count = count - 1; right}
-          | n when n < 0 -> Node {left = remove x left; value; count; right}
-          | _ -> Node {left; value; count; right = remove x right}
+  let rec find_opt x = function
+    | Empty -> None
+    | Node { left; value; count; right } ->
+      (match Ord.compare x value with
+       | 0 -> Some count
+       | n when n < 0 -> find_opt x left
+       | _ -> find_opt x right)
+  ;;
 
-    let rec map f = function
-      | Empty -> Empty
-      | Node {left; value; count; right} as t ->
-        let l' = map f left in
-        let v' = f value in
-        let r' = map f right in
-        if left == l' && value == v' && right == r' then t
-        else Node {left = l'; value = v'; count; right = r'}
+  let merge t1 t2 =
+    let t2_values = elements t2 in
+    let add_to_tree t k = add k t in
+    List.fold_left add_to_tree t1 t2_values
+  ;;
 
-    let rec filter p = function
-      | Empty -> Empty
-      | Node {left; value; count; right} as t ->
-        let l' = filter p left in
-        let pv = p value in
-        let r' = filter p right in
-        if pv then
-          if left == l' && right == r' then t
-          else Node {left = l'; value; count; right = r'}
-        else merge l' r'
+  let rec remove x = function
+    | Empty -> Empty
+    | Node { left; value; count; right } ->
+      (match Ord.compare x value with
+       | 0 ->
+         if count = 1
+         then merge left right
+         else Node { left; value; count = count - 1; right }
+       | n when n < 0 -> Node { left = remove x left; value; count; right }
+       | _ -> Node { left; value; count; right = remove x right })
+  ;;
 
-    let rec fold_right f bag accu = match bag with
-      | Empty -> accu
-      | Node {left; value; count; right} ->
-          fold_right f left (f value count (fold_right f right accu))
+  let rec map f = function
+    | Empty -> Empty
+    | Node { left; value; count; right } as t ->
+      let l' = map f left in
+      let v' = f value in
+      let r' = map f right in
+      if left == l' && value == v' && right == r'
+      then t
+      else Node { left = l'; value = v'; count; right = r' }
+  ;;
 
-    let rec fold_left f accu s =
+  let rec filter p = function
+    | Empty -> Empty
+    | Node { left; value; count; right } as t ->
+      let l' = filter p left in
+      let pv = p value in
+      let r' = filter p right in
+      if pv
+      then
+        if left == l' && right == r'
+        then t
+        else Node { left = l'; value; count; right = r' }
+      else merge l' r'
+  ;;
+
+  let rec fold_right f bag accu =
+    match bag with
+    | Empty -> accu
+    | Node { left; value; count; right } ->
+      fold_right f left (f value count (fold_right f right accu))
+  ;;
+
+  let rec fold_left f accu s =
     match s with
     | Empty -> accu
-    | Node {left; value; count; right} ->
-        fold_left f (f (fold_left f accu left) value count) right
+    | Node { left; value; count; right } ->
+      fold_left f (f (fold_left f accu left) value count) right
+  ;;
 end
--- test/test_bt_bag.ml
+++ test/test_bt_bag.ml
@@ -1,12 +1,14 @@
 open Alcotest
 
-module IntBag = Bt_bag.Make(struct
+module IntBag = Bt_bag.Make (struct
     type t = int
+
     let compare = compare
   end)
 
-module CharBag = Bt_bag.Make(struct
+module CharBag = Bt_bag.Make (struct
     type t = char
+
     let compare = compare
   end)
 
@@ -18,14 +20,16 @@ let test_add_case () =
   let bag = IntBag.add 3 bag in
   let bag = IntBag.add 2 bag in
   let result = IntBag.elements bag in
-  let expected = [1; 1; 2; 2; 3] in
+  let expected = [ 1; 1; 2; 2; 3 ] in
   check (list int) "same elements in add" expected result
+;;
 
 let test_is_empty_case () =
   let bag = IntBag.empty in
   check bool "empty bag" true (IntBag.is_empty bag);
   let bag = IntBag.add 1 bag in
   check bool "non-empty bag" false (IntBag.is_empty bag)
+;;
 
 let test_find_case () =
   let bag = IntBag.empty in
@@ -38,6 +42,7 @@ let test_find_case () =
   check int "find 2" 2 (IntBag.find 2 bag);
   check int "find 3" 1 (IntBag.find 3 bag);
   check_raises "find 4" Not_found (fun () -> ignore (IntBag.find 4 bag))
+;;
 
 let test_find_opt_case () =
   let bag = IntBag.empty in
@@ -50,6 +55,7 @@ let test_find_opt_case () =
   check (option int) "find 2" (Some 2) (IntBag.find_opt 2 bag);
   check (option int) "find 3" (Some 1) (IntBag.find_opt 3 bag);
   check (option int) "find 4" None (IntBag.find_opt 4 bag)
+;;
 
 let test_merge () =
   let bag1 = IntBag.empty in
@@ -66,8 +72,9 @@ let test_merge () =
   let bag2 = IntBag.add 3 bag2 in
   let bag = IntBag.merge bag1 bag2 in
   let result = IntBag.elements bag in
-  let expected = [1; 1; 1; 2; 2; 2; 2; 3; 3; 3] in
+  let expected = [ 1; 1; 1; 2; 2; 2; 2; 3; 3; 3 ] in
   check (list int) "same elements in merge" expected result
+;;
 
 let test_remove () =
   let bag = IntBag.empty in
@@ -78,8 +85,9 @@ let test_remove () =
   let bag = IntBag.add 2 bag in
   let bag = IntBag.remove 1 bag in
   let result = IntBag.elements bag in
-  let expected = [1; 2; 2; 3] in
+  let expected = [ 1; 2; 2; 3 ] in
   check (list int) "same elements in remove" expected result
+;;
 
 let test_map () =
   let bag = IntBag.empty in
@@ -90,8 +98,9 @@ let test_map () =
   let bag = IntBag.add 2 bag in
   let bag = IntBag.map (fun x -> x + 1) bag in
   let result = IntBag.elements bag in
-  let expected = [2; 2; 3; 3; 4] in
+  let expected = [ 2; 2; 3; 3; 4 ] in
   check (list int) "same elements in map" expected result
+;;
 
 let test_filter () =
   let bag = IntBag.empty in
@@ -102,8 +111,9 @@ let test_filter () =
   let bag = IntBag.add 2 bag in
   let bag = IntBag.filter (fun x -> x mod 2 = 0) bag in
   let result = IntBag.elements bag in
-  let expected = [2; 2] in
+  let expected = [ 2; 2 ] in
   check (list int) "same elements in filter" expected result
+;;
 
 let test_fold_right () =
   let bag = IntBag.empty in
@@ -112,117 +122,122 @@ let test_fold_right () =
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 3 bag in
   let bag = IntBag.add 2 bag in
-  let result = IntBag.fold_right (fun key count acc -> acc + key * count) bag 0 in
-  let expected = 1 * 2 + 2 * 2 + 3 * 1 in
+  let result = IntBag.fold_right (fun key count acc -> acc + (key * count)) bag 0 in
+  let expected = (1 * 2) + (2 * 2) + (3 * 1) in
   check int "same elements in fold_right" expected result
+;;
 
 let test_fold_right_zero_elements () =
   let bag = IntBag.empty in
-  let result = IntBag.fold_right (fun key count acc -> acc + key * count) bag 0 in
+  let result = IntBag.fold_right (fun key count acc -> acc + (key * count)) bag 0 in
   let expected = 0 in
   check int "same elements in fold right with no elements" expected result
+;;
 
 let test_fold_left_two_elements () =
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
-  let result = IntBag.fold_left (fun acc key count -> acc - key * count) 100 bag in
-  let expected = ((100-1)-2) in
-  check int "same elements in fold_left with two elements with substraction function" expected result
+  let result = IntBag.fold_left (fun acc key count -> acc - (key * count)) 100 bag in
+  let expected = 100 - 1 - 2 in
+  check
+    int
+    "same elements in fold_left with two elements with substraction function"
+    expected
+    result
+;;
 
-  let test_fold_left_three_elements () =
+let test_fold_left_three_elements () =
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
   let bag = IntBag.add 3 bag in
-  let result = IntBag.fold_left (fun acc key count -> acc - key * count) 100 bag in
-  let expected = ((100-1)-2)-3 in
-  check int "same elements in fold_left with three elements with substraction function" expected result
+  let result = IntBag.fold_left (fun acc key count -> acc - (key * count)) 100 bag in
+  let expected = 100 - 1 - 2 - 3 in
+  check
+    int
+    "same elements in fold_left with three elements with substraction function"
+    expected
+    result
+;;
 
 let test_fold_right_two_elements () =
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
-  let result = IntBag.fold_right (fun acc key count -> acc - key * count) bag 100 in
+  let result = IntBag.fold_right (fun acc key count -> acc - (key * count)) bag 100 in
   let expected = 1 - (2 - 100) in
-  check int "same elements in fold_right with two elements with substraction function" expected result
+  check
+    int
+    "same elements in fold_right with two elements with substraction function"
+    expected
+    result
+;;
 
-  let test_fold_right_three_elements () =
+let test_fold_right_three_elements () =
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
   let bag = IntBag.add 3 bag in
-  let result = IntBag.fold_right (fun acc key count -> acc - key * count) bag 100 in
+  let result = IntBag.fold_right (fun acc key count -> acc - (key * count)) bag 100 in
   let expected = 1 - (2 - (3 - 100)) in
-  check int "same elements in fold_right with three elements with substraction function" expected result
+  check
+    int
+    "same elements in fold_right with three elements with substraction function"
+    expected
+    result
+;;
 
-let test_fold_left () = 
+let test_fold_left () =
   let bag = IntBag.empty in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 2 bag in
   let bag = IntBag.add 1 bag in
   let bag = IntBag.add 3 bag in
   let bag = IntBag.add 2 bag in
-  let result = IntBag.fold_left (fun acc key count -> acc + key * count) 0 bag in
-  let expected = 1 * 2 + 2 * 2 + 3 * 1 in
+  let result = IntBag.fold_left (fun acc key count -> acc + (key * count)) 0 bag in
+  let expected = (1 * 2) + (2 * 2) + (3 * 1) in
   check int "same elements" expected result
+;;
 
 let test_fold_left_right_difference_case () =
   let bag = IntBag.empty in
   let bag = IntBag.add 5 bag in
   let bag = IntBag.add 10 bag in
   let bag = IntBag.add 15 bag in
-  let result_left = IntBag.fold_left (fun acc key count -> acc - key * count) 0 bag in
-  let result_right = IntBag.fold_right (fun acc key count -> acc - key * count) bag 0 in
-  let expected_left = ((0 - 5) - 10) - 15 in
+  let result_left = IntBag.fold_left (fun acc key count -> acc - (key * count)) 0 bag in
+  let result_right = IntBag.fold_right (fun acc key count -> acc - (key * count)) bag 0 in
+  let expected_left = 0 - 5 - 10 - 15 in
   let expected_right = 5 - (10 - (15 - 0)) in
   check int "same left" expected_left result_left;
   check int "same right" expected_right result_right
+;;
 
 let () =
-  run "bt_bag tests" [
-    (
-      "test_add",
-      [test_case "test_add" `Quick test_add_case]
-    );
-    (
-      "test_is_empty",
-      [test_case "test_is_empty" `Quick test_is_empty_case]
-    );
-    (
-      "test_find",
-      [
-       test_case "test_find" `Quick test_find_case;
-       test_case "test_find_opt" `Quick test_find_opt_case
-      ]
-    );
-    (
-      "test_merge",
-      [test_case "test_merge" `Quick test_merge]
-    );
-    (
-      "test_remove",
-      [test_case "test_remove" `Quick test_remove]
-    );
-    (
-      "test_map",
-      [test_case "test_map" `Quick test_map]
-    );
-    (
-      "test_filter",
-      [test_case "test_filter" `Quick test_filter]
-    );
-    (
-      "test_fold",
-      [
-        test_case "test_fold_right" `Quick test_fold_right;
-        test_case "test_fold_right_zero_elements" `Quick test_fold_right_zero_elements;
-        test_case "test_fold_right_two_elements" `Quick test_fold_right_two_elements;
-        test_case "test_fold_right_three_elements" `Quick test_fold_right_three_elements;
-        test_case "test_fold_left_two_elements" `Quick test_fold_left_two_elements;
-        test_case "test_fold_left_three_elements" `Quick test_fold_left_three_elements;
-        test_case "test_fold_left" `Quick test_fold_left;
-        test_case "test_fold_left_right_difference" `Quick test_fold_left_right_difference_case
-      ]
-    )
-  ]
+  run
+    "bt_bag tests"
+    [ "test_add", [ test_case "test_add" `Quick test_add_case ]
+    ; "test_is_empty", [ test_case "test_is_empty" `Quick test_is_empty_case ]
+    ; ( "test_find"
+      , [ test_case "test_find" `Quick test_find_case
+        ; test_case "test_find_opt" `Quick test_find_opt_case
+        ] )
+    ; "test_merge", [ test_case "test_merge" `Quick test_merge ]
+    ; "test_remove", [ test_case "test_remove" `Quick test_remove ]
+    ; "test_map", [ test_case "test_map" `Quick test_map ]
+    ; "test_filter", [ test_case "test_filter" `Quick test_filter ]
+    ; ( "test_fold"
+      , [ test_case "test_fold_right" `Quick test_fold_right
+        ; test_case "test_fold_right_zero_elements" `Quick test_fold_right_zero_elements
+        ; test_case "test_fold_right_two_elements" `Quick test_fold_right_two_elements
+        ; test_case "test_fold_right_three_elements" `Quick test_fold_right_three_elements
+        ; test_case "test_fold_left_two_elements" `Quick test_fold_left_two_elements
+        ; test_case "test_fold_left_three_elements" `Quick test_fold_left_three_elements
+        ; test_case "test_fold_left" `Quick test_fold_left
+        ; test_case
+            "test_fold_left_right_difference"
+            `Quick
+            test_fold_left_right_difference_case
+        ] )
+    ]
+;;
