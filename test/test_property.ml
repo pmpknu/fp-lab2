4d97f13d5c71c40c67a6cff584769f126a768997
pmpknu <dangorlyakov@gmail.com>
2024-12-23 23:16:05 +0300

    fix: still try to start flow

--- test/test_property.ml
+++ test/test_property.ml
@@ -1,17 +1,15 @@
 module IntBag = Bt_bag.Make (struct
-  type t = int
-  let compare = compare
-end)
+    type t = int
 
-let prop_reverse lst = (lst) = lst;;
+    let compare = compare
+  end)
+
+let prop_reverse lst = lst = lst
 
 open QCheck
 
 let test =
-  QCheck.Test.make
-  ~count:1000
-  ~name:"reverse"
-  (list small_int)
-  (fun x -> prop_reverse x);;
+  QCheck.Test.make ~count:1000 ~name:"reverse" (list small_int) (fun x -> prop_reverse x)
+;;
 
-QCheck_runner.run_tests ~verbose:true [test];;
+QCheck_runner.run_tests ~verbose:true [ test ]
